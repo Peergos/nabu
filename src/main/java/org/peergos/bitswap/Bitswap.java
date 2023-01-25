039e996d2e1fff178eb934a2a014cb769a3762f1
ian <ianopolous@protonmail.com>
2023-01-25 09:48:12 +0000

    Simplify Bitswap classes

--- src/main/java/org/peergos/bitswap/Bitswap.java
+++ src/main/java/org/peergos/bitswap/Bitswap.java
@@ -1,12 +1,14 @@
 package org.peergos.bitswap;
 
-public class Bitswap extends BitswapBinding {
+import io.libp2p.core.multistream.*;
+
+public class Bitswap extends StrictProtocolBinding<BitswapController> {
     public static int MAX_MESSAGE_SIZE = 2*1024*1024;
 
     private final BitswapEngine engine;
 
     public Bitswap(BitswapEngine engine) {
-        super(new BitswapProtocol(engine));
+        super("/ipfs/bitswap/1.2.0", new BitswapProtocol(engine));
         this.engine = engine;
     }
 
