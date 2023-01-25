16bc558c9f03556c34e42370e57f84d5b0c7e005
ian <ianopolous@protonmail.com>
2023-01-25 13:44:03 +0000

    Use the dht which kubo is advertising in test

--- src/test/java/org/peergos/KademliaTest.java
+++ src/test/java/org/peergos/KademliaTest.java
@@ -1,6 +1,7 @@
 package org.peergos;
 
 import com.google.protobuf.*;
+import identify.pb.*;
 import io.ipfs.api.*;
 import io.ipfs.cid.*;
 import io.ipfs.multihash.Multihash;
@@ -20,9 +21,10 @@ public class KademliaTest {
     @Test
     public void bootstrap() throws Exception {
         Bitswap bitswap1 = new Bitswap(new BitswapEngine(new RamBlockstore()));
-        Kademlia dht = new Kademlia(new KademliaEngine(), true);
+        Kademlia lanDht = new Kademlia(new KademliaEngine(), true);
+        Kademlia wanDht = new Kademlia(new KademliaEngine(), false);
         Ping ping = new Ping();
-        Host node1 = Server.buildHost(10000 + new Random().nextInt(50000), List.of(ping, bitswap1, dht));
+        Host node1 = Server.buildHost(10000 + new Random().nextInt(50000), List.of(ping, bitswap1, lanDht, wanDht));
         node1.start().join();
         try {
             IPFS kubo = new IPFS("localhost", 5001);
@@ -31,6 +33,8 @@ public class KademliaTest {
             byte[] hash = new byte[32];
             new Random(42).nextBytes(hash);
             Cid nodeToFind = new Cid(1, Cid.Codec.Libp2pKey, Multihash.Type.sha2_256, hash);
+            IdentifyOuterClass.Identify id = new Identify().dial(node1, address2).getController().join().id().join();
+            Kademlia dht = id.getProtocolsList().contains("/ipfs/lan/kad/1.0.0") ? lanDht : wanDht;
             KademliaController bootstrap = dht.dial(node1, address2).getController().join();
             bootstrap.send(Dht.Message.newBuilder()
                     .setType(Dht.Message.MessageType.FIND_NODE)
