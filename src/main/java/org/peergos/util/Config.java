19733592945c3e4b3b64d2ffc1dcb647093bdfe6
kevodwyer <kevodwyer@gmail.com>
2023-02-28 09:55:58 +0000

    add tighter type restrictions to Config

--- src/main/java/org/peergos/util/Config.java
+++ src/main/java/org/peergos/util/Config.java
@@ -6,13 +6,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Config {
-    private final Map configuration;
-    public Config(Map jsonMap) {
+    private final Map<String, Object> configuration;
+    public Config(Map<String, Object> jsonMap) {
         this.configuration = jsonMap;
     }
 
     public String getProperty(String... propNames) {
-        Map currentMap = traverseProperties(Arrays.stream(propNames).limit(propNames.length -1).collect(Collectors.toList()));
+        Map<String, Object> currentMap = traverseProperties(Arrays.stream(propNames).limit(propNames.length -1).collect(Collectors.toList()));
         String lastProperty = propNames[propNames.length -1];
         if(! currentMap.containsKey(lastProperty)) {
             throw new IllegalStateException("Property not found:" + lastProperty);
@@ -23,8 +23,8 @@ public class Config {
         }
         return (String)currentMap.get(lastProperty);
     }
-    public List getPropertyList(String... propNames) {
-        Map currentMap = traverseProperties(Arrays.stream(propNames).limit(propNames.length -1).collect(Collectors.toList()));
+    public List<Map> getPropertyList(String... propNames) {
+        Map<String, Object> currentMap = traverseProperties(Arrays.stream(propNames).limit(propNames.length -1).collect(Collectors.toList()));
         String lastProperty = propNames[propNames.length -1];
         if(! currentMap.containsKey(lastProperty)) {
             throw new IllegalStateException("Property not found:" + lastProperty);
@@ -33,10 +33,10 @@ public class Config {
         if (! (obj instanceof List)) {
             throw new IllegalStateException("Not a List property:" + lastProperty);
         }
-        return (List)currentMap.get(lastProperty);
+        return (List<Map>)currentMap.get(lastProperty);
     }
-    public Map getPropertyMap(String... propNames) {
-        Map currentMap = traverseProperties(Arrays.stream(propNames).limit(propNames.length -1).collect(Collectors.toList()));
+    public Map<String, Object> getPropertyMap(String... propNames) {
+        Map<String, Object> currentMap = traverseProperties(Arrays.stream(propNames).limit(propNames.length -1).collect(Collectors.toList()));
         String lastProperty = propNames[propNames.length -1];
         if(! currentMap.containsKey(lastProperty)) {
             throw new IllegalStateException("Property not found:" + lastProperty);
@@ -45,12 +45,12 @@ public class Config {
         if (! (obj instanceof Map)) {
             throw new IllegalStateException("Not a List property:" + lastProperty);
         }
-        return (Map)currentMap.get(lastProperty);
+        return (Map<String, Object>)currentMap.get(lastProperty);
     }
     public Map traverseProperties(List<String> propNames) {
-        Map currentMap = configuration;
+        Map<String, Object> currentMap = configuration;
         for (String propName : propNames) {
-            currentMap = (Map)currentMap.get(propName);
+            currentMap = (Map<String, Object>)currentMap.get(propName);
             if (currentMap == null) {
                 throw new IllegalStateException("Property not found:" + propName);
             }
