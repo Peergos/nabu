dbc45048de7c30dba8d14a208c518fa76e84eb7d
Boris Velkovski <boris@Boriss-MacBook-Pro.local>
2023-03-29 14:31:24 +0300

    Fix tests

--- src/test/java/org/peergos/KademliaTest.java
+++ src/test/java/org/peergos/KademliaTest.java
@@ -15,12 +15,12 @@ public class KademliaTest {
     public void findOtherNode() throws Exception {
         RamBlockstore blockstore1 = new RamBlockstore();
         HostBuilder builder1 = HostBuilder.build(10000 + new Random().nextInt(50000),
-                new RamProviderStore(), new RamRecordStore(), blockstore1, (c, b, p, a) -> CompletableFuture.completedFuture(true));
+                new RamProviderStore(), new RamRecordStore(), blockstore1, (c, b, p, a) -> CompletableFuture.completedFuture(true), false);
         Host node1 = builder1.build();
         node1.start().join();
 
         HostBuilder builder2 = HostBuilder.build(10000 + new Random().nextInt(50000),
-                new RamProviderStore(), new RamRecordStore(), new RamBlockstore(), (c, b, p, a) -> CompletableFuture.completedFuture(true));
+                new RamProviderStore(), new RamRecordStore(), new RamBlockstore(), (c, b, p, a) -> CompletableFuture.completedFuture(true), false);
         Host node2 = builder2.build();
         node2.start().join();
 
