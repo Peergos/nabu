7bf273403faa48a093bbd7f8764a35a52a959d91
kevodwyer <kevodwyer@gmail.com>
2023-05-22 12:33:46 +0100

    wip

--- src/test/java/org/peergos/PingTest.java
+++ src/test/java/org/peergos/PingTest.java
@@ -32,4 +32,25 @@ public class PingTest {
             node2.stop();
         }
     }
+    @Test
+    public void runPingTest() {
+        Host node2 = HostBuilder.build(11002, List.of(new Ping(), new Bitswap(new BitswapEngine(new RamBlockstore(), (c, b, p, a) -> CompletableFuture.completedFuture(true)))));
+        node2.start().join();
+        try {
+            //Multiaddr address = Multiaddr.fromString("/ip4/0.0.0.0/tcp/4001");
+            Multiaddr address = Multiaddr.fromString("/ip4/127.0.0.1/tcp/4001");
+            String peerIdStr = "12D3KooWA24mDfWkWRx2K3ERn6KKMfJbqxNjccVSQQdkCWUkjQZs"; //target.peerId.toBase58()
+            PeerId peerId = PeerId.fromBase58(peerIdStr);
+            PingController pinger = new Ping().dial(node2, peerId, address).getController().join();
+            System.out.println("Sending ping messages to " + address);
+            for (int i = 0; i < 2; i++) {
+                long latency = pinger.ping().join();
+                System.out.println("Ping " + i + ", latency " + latency + "ms");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            node2.stop();
+        }
+    }
 }
