cff7254f03687ae14a7629c6aee53e6b048a9496
ian <ianopolous@protonmail.com>
2023-05-31 16:01:02 +0100

    Update to new mostly-upstreamed jvm-libp2p

--- src/main/java/org/peergos/HostBuilder.java
+++ src/main/java/org/peergos/HostBuilder.java
@@ -155,8 +155,8 @@ public class HostBuilder {
         Host host = BuilderJKt.hostJ(Builder.Defaults.None, b -> {
             b.getIdentity().setFactory(() -> privKey);
             b.getTransports().add(TcpTransport::new);
-            b.getSecureChannels().add((k, m) -> new NoiseXXSecureChannel(k, (List<String>)m));
-            b.getSecureChannels().add((k, m) -> new TlsSecureChannel(k, (List<String>)m));
+            b.getSecureChannels().add((k, m) -> new NoiseXXSecureChannel(k, m));
+            b.getSecureChannels().add((k, m) -> new TlsSecureChannel(k, m));
             b.getMuxers().addAll(muxers);
             b.getAddressBook().setImpl(new RamAddressBook());
 
