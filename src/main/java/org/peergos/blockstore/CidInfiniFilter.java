a13f0c35aa43f88d1ed541c327ac0c93977a33d3
ian <ianopolous@protonmail.com>
2023-09-28 10:50:28 +0100

    Log Infini filter ram size during construction

--- src/main/java/org/peergos/blockstore/CidInfiniFilter.java
+++ src/main/java/org/peergos/blockstore/CidInfiniFilter.java
@@ -30,10 +30,11 @@ public class CidInfiniFilter implements Filter {
 
     public static CidInfiniFilter build(Blockstore bs, double falsePositiveRate) {
         List<Cid> refs = bs.refs().join();
-        int nBlocks = refs.size();
+        int nBlocks = refs.size()*5/4; //  increase by 25% to avoid expansion during build
         int nextPowerOfTwo = Math.max(17, (int) (1 + Math.log(nBlocks) / Math.log(2)));
         double expansionAlpha = 0.8;
         int bitsPerEntry = (int)(4 - Math.log(falsePositiveRate / expansionAlpha) / Math.log(2) + 1);
+        System.out.println("Using infini filter of initial size " + ((double)(bitsPerEntry * (1 << nextPowerOfTwo) / 8) / 1024 / 1024) + " MiB");
         ChainedInfiniFilter infini = new ChainedInfiniFilter(nextPowerOfTwo, bitsPerEntry);
         infini.set_expand_autonomously(true);
         refs.forEach(c -> infini.insert(c.toBytes(), true));
