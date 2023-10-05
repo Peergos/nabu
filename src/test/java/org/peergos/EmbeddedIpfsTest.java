b6090ece369f82727cd0e143e10e282d86ade4fc
ian <ianopolous@protonmail.com>
2023-10-05 14:37:45 +0100

    Fix addresses we announce via Identify
    If we are listening on a wildcard address we need to expand it,
    and find all the matching interfaces and their ip addresses.
    
    Add a test to verify this to EmbeddedIpfsTest

--- src/test/java/org/peergos/EmbeddedIpfsTest.java
+++ src/test/java/org/peergos/EmbeddedIpfsTest.java
@@ -1,9 +1,12 @@
 package org.peergos;
 
+import identify.pb.*;
 import io.ipfs.cid.*;
 import io.ipfs.multiaddr.*;
 import io.libp2p.core.*;
 import io.libp2p.core.crypto.*;
+import io.libp2p.core.multiformats.*;
+import io.libp2p.protocol.*;
 import org.junit.*;
 import org.peergos.blockstore.*;
 import org.peergos.config.*;
@@ -17,12 +20,12 @@ public class EmbeddedIpfsTest {
 
     @Test
     public void largeBlock() throws Exception {
-        EmbeddedIpfs node1 = build(Collections.emptyList());
+        EmbeddedIpfs node1 = build(Collections.emptyList(), List.of(new MultiAddress("/ip4/127.0.0.1/tcp/" + TestPorts.getPort())));
         node1.start();
         EmbeddedIpfs node2 = build(node1.node.listenAddresses()
                 .stream()
                 .map(a -> new MultiAddress(a.toString()))
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList()), List.of(new MultiAddress("/ip4/127.0.0.1/tcp/" + TestPorts.getPort())));
         node2.start();
 
         Cid block = node2.blockstore.put(new byte[1024 * 1024], Cid.Codec.Raw).join();
@@ -36,9 +39,26 @@ public class EmbeddedIpfsTest {
         node2.stop();
     }
 
-    public static EmbeddedIpfs build(List<MultiAddress> bootstrap) {
-        int swarm = TestPorts.getPort();
-        List<MultiAddress> swarmAddresses = List.of(new MultiAddress("/ip4/127.0.0.1/tcp/" + swarm));
+    @Test
+    public void wildcardListenerAddressesGetExpanded() {
+        int node1Port = TestPorts.getPort();
+        EmbeddedIpfs node1 = build(Collections.emptyList(), List.of(new MultiAddress("/ip6/::/tcp/" + node1Port)));
+        node1.start();
+
+        EmbeddedIpfs node2 = build(node1.node.listenAddresses()
+                .stream()
+                .map(a -> new MultiAddress(a.toString()))
+                .collect(Collectors.toList()), List.of(new MultiAddress("/ip4/127.0.0.1/tcp/" + TestPorts.getPort())));
+        node2.start();
+        Multiaddr node1Addr = new Multiaddr("/ip4/127.0.0.1/tcp/" + node1Port + "/p2p/" + node1.node.getPeerId());
+        IdentifyOuterClass.Identify id = new Identify().dial(node2.node, node1Addr).getController().join().id().join();
+        List<MultiAddress> listening = id.getListenAddrsList().stream().map(b -> new MultiAddress(b.toByteArray())).collect(Collectors.toList());
+        Assert.assertTrue(listening.stream().anyMatch(a -> a.toString().contains("/ip4/127.0.0.1")));
+        Assert.assertTrue(listening.stream().noneMatch(a -> a.toString().contains("/p2p/")));
+        Assert.assertTrue(listening.stream().noneMatch(a -> a.toString().contains("/ipfs/")));
+    }
+
+    public static EmbeddedIpfs build(List<MultiAddress> bootstrap, List<MultiAddress> swarmAddresses) {
         BlockRequestAuthoriser blockRequestAuthoriser = (c, b, p, a) -> CompletableFuture.completedFuture(true);
         HostBuilder builder = new HostBuilder().generateIdentity();
         PrivKey privKey = builder.getPrivateKey();
