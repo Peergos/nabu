7658f321ac3f54e83f4d21c7150236e360afec6d
ian <ianopolous@protonmail.com>
2023-10-17 10:16:25 +0100

    Revert change to not error when deleting a block we don't have.
    This is useful information downstream.

--- src/main/java/org/peergos/net/APIHandler.java
+++ src/main/java/org/peergos/net/APIHandler.java
@@ -124,11 +124,19 @@ public class APIHandler extends Handler {
                         throw new APIException("argument \"cid\" is required\n");
                     }
                     Cid cid = Cid.decode(args.get(0));
-                    ipfs.blockstore.rm(cid).join();
-                    Map res = new HashMap<>();
-                    res.put("Error", "");
-                    res.put("Hash", cid.toString());
-                    replyJson(httpExchange, JSONParser.toString(res));
+                    boolean deleted = ipfs.blockstore.rm(cid).join();
+                    if (deleted) {
+                        Map res = new HashMap<>();
+                        res.put("Error", "");
+                        res.put("Hash", cid.toString());
+                        replyJson(httpExchange, JSONParser.toString(res));
+                    } else {
+                        try {
+                            httpExchange.sendResponseHeaders(400, 0);
+                        } catch (IOException ioe) {
+                            HttpUtil.replyError(httpExchange, ioe);
+                        }
+                    }
                     break;
                 }
                 case STAT: { // https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-block-stat
