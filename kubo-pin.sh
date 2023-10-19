59cc0617c4a1c368bf0c4edbf63c1fe8595bb3a0
ian <ianopolous@protonmail.com>
2023-10-19 21:48:17 +0100

    Dial provider before kubo pin

--- kubo-pin.sh
+++ kubo-pin.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 export PATH=$PATH:$PWD/kubo/
+ipfs swarm connect /ip4/172.104.157.121/tcp/4001/p2p/QmVdFZgHnEgcedCS2G2ZNiEN59LuVrnRm7z3yXtEBv2XiF
 time ipfs pin add zdpuAwfJrGYtiGFDcSV3rDpaUrqCtQZRxMjdC6Eq9PNqLqTGg
